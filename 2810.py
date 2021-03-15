4e7d000659a7b28d2671b0aea4c4b9b0e86ff5db
Gun1Yun <rjsdlf45@gmail.com>
2021-03-15 14:52:37 +0900

    solving BOJ2810

---
+++ 2810.py
@@ -0,0 +1,27 @@
+import sys
+In = sys.stdin.readline
+
+
+def main():
+    line = int(In())
+    seats = In().rstrip()
+
+    cnt = 1
+    couple = 0
+
+    for seat in seats:
+        if seat == 'S':
+            cnt += 1
+
+        if seat == 'L':
+            if couple == 0:
+                couple += 1
+            else:
+                couple = 0
+                cnt += 1
+
+    print(cnt)
+
+
+if __name__ == "__main__":
+    main()
